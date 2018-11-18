a8e2e4e558cf6b294f29f333506b866cbcec4986
martlandh <HarryMartland@Rentalcars.com>
2018-11-18 16:46:02 +0000

    removed aws config

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const AWS = require('aws-sdk');
 const process = require("./src/process");
 
 AWS.config.region = 'eu-west-1';
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY
-});
 
 exports.handler = function (event, context, callback) {
     return process()
